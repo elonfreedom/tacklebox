ae46bb54b6a25a1875d619df3fba251b287bc1be
elonfreedom <elonfreedom@qq.com>
2025-12-06 19:04:32 +0800

    Initial Commit

---
+++ TackleBox/TackleBoxApp.swift
@@ -0,0 +1,32 @@
+//
+//  TackleBoxApp.swift
+//  TackleBox
+//
+//  Created by elonfreedom on 2025/12/6.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TackleBoxApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TackleBoxTests/TackleBoxTests.swift
@@ -0,0 +1,17 @@
+//
+//  TackleBoxTests.swift
+//  TackleBoxTests
+//
+//  Created by elonfreedom on 2025/12/6.
+//
+
+import Testing
+@testable import TackleBox
+
+struct TackleBoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
